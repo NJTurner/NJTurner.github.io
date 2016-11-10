5db715bab9bdc0720b2a34de970d164b00c2757a
Nick Turner <dimesteaksNT@gmail.com>
2016-11-10 14:15:59 -0600

    changed size of carousel img on index

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,15 @@
 // Empty JS for your own code to be here
+'use strict';
+
+$(document).keydown(function(e) {
+    if (e.keyCode === 37) {
+        // Previous
+        $(".carousel-control.left").click();
+        return false;
+    }
+    if (e.keyCode === 39) {
+        // Next
+        $(".carousel-control.right").click();
+        return false;
+    }
+});
